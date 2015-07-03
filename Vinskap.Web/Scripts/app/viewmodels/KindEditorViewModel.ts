e7dc1211087415eb32c5632baf1f857cf6a10f37
Andrea Dallera <andrea@drofus.no>
2015-07-03 13:53:17 +0200

    edit wine type

--- Vinskap.Web/Scripts/app/viewmodels/KindEditorViewModel.ts
+++ Vinskap.Web/Scripts/app/viewmodels/KindEditorViewModel.ts
@@ -3,12 +3,20 @@ class KindEditorViewModel extends ViewModel {
 
     kind: Kind;
     name: KnockoutObservable<string>;
-    wineType: KnockoutObservable<string>;
+    wineType: KnockoutObservable<string>;        
 
     constructor(searchTerm: string) {
         super("KindEditorView");
         this.kind = Kind.fromSearchTerm(searchTerm);
         this.name = ko.observable(this.kind.Name);
         this.wineType = ko.observable(this.kind.Type);
+    }
+
+    isSelected(wineType: string) {
+        return this.wineType() == wineType;
+    }
+
+    selectType(wineType: string) {
+        this.wineType(wineType);
     }
 }  
